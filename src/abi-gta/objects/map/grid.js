37a8a3d0829413d458810f38a8838a8a725919c5
Vo Van Kiet <vokiet.15111999@gmail.com>
2023-11-13 11:00:16 +0700

    +create grid

--- src/abi-gta/objects/map/grid.js
+++ src/abi-gta/objects/map/grid.js
@@ -1,28 +1,26 @@
 import { Entity } from "playcanvas";
+import { AssetLoader } from "../../../assetLoader/assetLoader";
 import { Brick } from "./brick";
-import { DataManager } from "../../data/dataManager";
+import mapData from "/assets/jsons/mapData.json"
 export class Grid extends Entity{
   constructor() {
     super("grid");
-    this.col = DataManager.mapData.length - 1;
-    this.row = DataManager.mapData[0].length - 1;
-    this.gridUnit = DataManager.mapUnit;
-    this.bricks = [];
+    this.data = mapData.mapData;
     this.generateGrid();
   }
 
   generateGrid() { 
-    for (let i = 0; i < DataManager.mapData.length; i++) {
-      let row = DataManager.mapData[i];
-      for (let j = 0; j < row.length; j++) {
+    for(let i = 0; i < this.data.length; i++){
+      let row = this.data[i];
+      for(let j = 0; j < row.length; j++){
         let tile = row[j];
-        if (tile === 0) {
-          let brick = new Brick();
-          brick.row = j;
-          brick.col = i;
-          brick.setLocalPosition(i * this.gridUnit, 0, j * this.gridUnit);
-          this.addChild(brick);
-          this.bricks.push(brick);
+        if(tile === 0){
+          let tileEntity = new Entity();
+          tileEntity.addComponent( "model", {
+            asset : AssetLoader.getAssetByKey("model_sidewalk")
+          })
+          tileEntity.setLocalPosition(i * mapData.unit, 0, j * mapData.unit);
+          this.addChild(tileEntity);
         }
       }
     }
--- src/abi-gta/objects/map/map.js
+++ src/abi-gta/objects/map/map.js
@@ -2,69 +2,16 @@ import { Entity, Vec3 } from "playcanvas";
 import { Brick } from "./brick";
 import { Building } from "./building";
 import { Crossing } from "./crossing";
+import { Grid } from "./grid";
 import { Road } from "./road";
 import { mapData } from "/assets/jsons/map1Data.json";
 
-export const MapItemCode = Object.freeze({
-  Road: 1,
-  Crossing: 2,
-  Brick: 0,
-  Building1: 3,
-  Building2: 4,
-  Building3: 5,
-});
+
 export class Map extends Entity{
   constructor() {
     super();
-    this._initBrick();
-    // this._initRoad();
+    this.grid = new Grid();
+    this.addChild(this.grid);
   }
 
-  _initBrick() {
-    for(let i = 0; i < mapData.length; i++) {
-      for(let j = 0; j < mapData[i].length; j++) {
-        if(mapData[i][j] === 0) {
-          this.brick = new Brick();
-          this.addChild(this.brick)
-          this.brick.setLocalPosition(i * 5, 0 ,j * 5)
-        }
-      }
-    }
-  }
-
-  _initRoad() {
-    for(let i = 0; i < mapData.length; i++) {
-      for(let j = 0; j < mapData[i].length; j++) {
-        if(mapData[i][j] === 1) {
-          this.road = new Road();
-          this.addChild(this.road)
-          this.road.setLocalPosition(i * 5, 0 ,j * 5)
-        }
-      }
-    }
-  }
-
-  _initCrossing() {
-    for(let i = 0; i < mapData.length; i++) {
-      for(let j = 0; j < mapData[i].length; j++) {
-        if(mapData[i][j] === 2) {
-          this.cross = new Crossing();
-          this.addChild(this.cross)
-          this.cross.setLocalPosition(i * 5, 0 ,j * 5)
-        }
-      }
-    }
-  }
-
-  _initBuilding() {
-    for(let i = 0; i < mapData.length; i++) {
-      for(let j = 0; j < mapData[i].length; j++) {
-        if(mapData[i][j] === 4) {
-          this.building = new Building();
-          this.addChild(this.building)
-          this.building.setLocalPosition(i * 5, 0 ,j * 5)
-        }
-      }
-    }
-  }
 }
